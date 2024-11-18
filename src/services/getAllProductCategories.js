6b4867d7fa6e26bceb654fb32604ddb523baf374
angeltafrd <itafirdaus9@gmail.com>
2024-11-18 20:44:33 +0700

    first commit

---
+++ src/services/getAllProductCategories.js
@@ -0,0 +1,13 @@
+export default function getAllProductCategories(){
+  return  [   {
+      id: 'FMEN',
+      slug: 'boy-group',
+      name: `Boy Group`
+    },
+    {
+      id: 'FWMEN',
+      slug: 'girl-group',
+      name: `Girl Group`
+    },
+  ]
+}
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,70 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'NAF-1-07-FMEN-001',
+      slug: 'aespa-armageddon-fisrt-full-length-album-girl',
+      name: "AESPA Armegeddon First Full Length Album",
+      category: `Girl Group`,
+      categorySlug: 'girl-group',
+      price: 1_875_000,
+      stock: 20,
+      imageUrl: '/assets/images/aespa armageddon album.jpeg',
+      description: 'Armageddon is Aespas first full-length album, released in 2024, marking a significant evolution in their sound and artistic identity. The album features 10 tracks that explore themes of self-discovery and transformation across multiple realms. With a mix of genres, from energetic hip-hop to retro synth-pop, Armageddon combines bold, catchy songs like Supernova and introspective tracks like Mine and Set the Tone. The album showcases the groups mature vocal performances and dynamic music, reinforcing Aespas unique position in the K-pop world and their growing influence​.'
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-002',
+      slug: 'orange-blood',
+      name: "ENHYPEN Orange Blood Album",
+      category: `Boy Group`,
+      categorySlug: 'boy-group',
+      price: 409_000,
+      stock: 10,
+      imageUrl: `/assets/images/Orange Blood enhypen álbum engene.jpeg`,
+      description: `Orange Blood is ENHYPENs highly anticipated album, marking a new chapter in the group's musical journey. The album explores a darker, more mature side of ENHYPEN, incorporating elements of rock, pop, and electronic music. Themes of passion, conflict, and transformation are woven throughout the tracks, with songs that delve into the complexities of youth and identity. The album features powerful vocals, intense choreography, and dynamic production, showcasing ENHYPEN's evolving sound and their ability to push boundaries. Orange Blood cements their place as one of the most innovative K-pop groups in the industry.`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-003',
+      slug: 'nj-getup',
+      name: "Newjeans Album-get up Full Version",
+      category: `Girl Group`,
+      categorySlug: 'girl-group',
+      price: 949_000,
+      stock: 1,
+      imageUrl: `/assets/images/Newjeans Album-get up.jpeg`,
+      description: `Get Up is NewJeans much-anticipated album, released in 2024, continuing their exploration of fresh sounds and unique storytelling. This album blends genres like R&B, pop, and alternative with their signature smooth melodies and experimental beats. The themes of Get Up revolve around self-expression, youth, and navigating emotions in a fast-paced world. With captivating tracks like "Get Up" and "Flicker," NewJeans showcases their growth as artists, experimenting with both introspective and bold sounds. Their playful yet mature vocal delivery, combined with innovative production, further solidifies NewJeans as trendsetters in the K-pop scene, captivating a wide range of listeners with their distinct sound.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-004',
+      slug: 'skz-maxi-usver',
+      name: "STRAY KIDS MINI ALBUM - MAXIDENT (US VER_) - GO",
+      category: `Boy Group`,
+      categorySlug: 'boy-group',
+      price: 509_000,
+      stock: 0,
+      imageUrl: `/assets/images/STRAY KIDS MINI ALBUM - MAXIDENT (US VER_) - GO.jpeg`,
+      description: `MAXIDENT (US VER.) is the special US version of Stray Kids explosive mini album MAXIDENT, released in 2024. This version features a mix of their high-energy tracks and powerful performances, combining intense hip-hop beats, electrifying synths, and emotional ballads. MAXIDENT explores themes of love, obsession, and the complexities of relationships, with bold and experimental sounds that push Stray Kids' musical boundaries. Known for their intricate production and dynamic vocals, Stray Kids continues to solidify their international influence with this version, which includes exclusive tracks and remixes tailored for the American market. With tracks like "Case 143" and "S-Class," Stray Kids brings their unique style to an even broader global audience.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-005',
+      slug: 'txt-ff',
+      name: "TXT - The Name Chapter:FREEFALL Full Version",
+      category: `Boy Group`,
+      categorySlug: 'boy-group',
+      price: 1_250_000,
+      stock: 2,
+      imageUrl: `/assets/images/TXT - The Name Chapter_ FREEFALL.jpeg`,
+      description: `The Name Chapter: FREEFALL is TXT highly anticipated album, released in 2024, marking a bold and exciting new era in their musical journey. This album delves into themes of self-exploration, freedom, and breaking boundaries. With a mix of soaring melodies, rich harmonies, and experimental production, TXT blends pop, rock, and electronic influences to create a unique and immersive sound. The album's tracks tackle the complexities of youth and the search for identity, featuring powerful performances from the groups members. With songs like "FREEFALL" and "Chasing Light," TXT continues to push the limits of K-pop, delivering emotionally charged lyrics and innovative music that resonates with listeners worldwide. The Name Chapter: FREEFALL reinforces TXT position as pioneers of the next generation of K-pop.`
+    },
+    {
+      id: 'NAF-1-07-FWMEN-LV8-003',
+      slug: 'rv-cosmic',
+      name: "Red Velvet Mini Album Vol. 9 - Cosmic (Photo Book Version) (Random Version)",
+      category: `Girl Group`,
+      categorySlug: 'girl-group',
+      price: 549_000,
+      stock: 0,
+      imageUrl: `https://i.pinimg.com/474x/87/b4/8d/87b48d07862b8be70bbeec00c4f1739c.jpg`,
+      description: `Cosmic is Red Velvet's 9th mini album, released in 2024, showcasing the group's signature blend of captivating vocals and genre-spanning music. This album takes listeners on a journey through cosmic-themed tracks that explore the ideas of love, mystery, and adventure. With elements of dreamy pop, R&B, and dance music, Cosmic highlights the group's artistic evolution and their ability to craft a unique sound that stands out in the K-pop landscape.`
+    },
+  ]
+}
